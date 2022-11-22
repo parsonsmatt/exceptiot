90bc365cec036ee3f5c9be1aab9be51791f17e5f
parsonsmatt <parsonsmatt@gmail.com>
2022-11-22 15:34:52 -0700

    different name

--- src/Control/Monad/Except/IO.hs
+++ src/Control/Monad/Except/IO.hs
@@ -3,7 +3,7 @@
 {-# language FlexibleInstances #-}
 {-# language MultiParamTypeClasses #-}
 
-module Control.Monad.ExceptIOT where
+module Control.Monad.Except.IO where
 
 import UnliftIO
 import Control.Applicative
@@ -12,12 +12,16 @@ import Control.Monad.State
 import Control.Monad.Writer
 import Control.Monad.Except
 
-
 newtype ExceptIOT e m a = ExceptIOT { unsafeRunExceptIOT :: m a }
     deriving newtype
         (Functor, Applicative, Monad, MonadIO, MonadPlus, Alternative,
-        Semigroup, Monoid, MonadUnliftIO, MonadReader r, MonadState s, MonadWriter w)
+        Semigroup, Monoid, MonadUnliftIO, MonadReader r, MonadState s, MonadWriter w, MonadFix, MonadFail)
+
+type ExceptIO e = ExceptIOT e IO
 
 instance (MonadUnliftIO m, Exception e) => MonadError e (ExceptIOT e m) where
     throwError = throwIO
     catchError = catch
+
+runExceptIOT :: (Exception e, MonadUnliftIO m) => ExceptIOT e m a -> m (Either e a)
+runExceptIOT = try . unsafeRunExceptIOT
